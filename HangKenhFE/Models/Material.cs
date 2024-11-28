2fdd9d4573b1012e6a14f5a4c2156b4135998077
Đức Chuẩn Nguyễn <nguyenducchuan2004@gmail.com>
2024-11-28 19:26:51 +0700

    Update home

--- HangKenhFE/Models/Material.cs
+++ HangKenhFE/Models/Material.cs
@@ -8,9 +8,9 @@ namespace HangKenhFE.Models
     {
         [Key]
         public long Id { get; set; }
-        public string Title { get; set; }
-        public string Slug { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Slug { get; set; }
+        public string? Description { get; set; }
         public bool? Deleted { get; set; }
         public DateTime Create_at { get; set; }
         public DateTime Update_at { get; set; }
--- HangKenhFE/Models/Product_Posts.cs
+++ HangKenhFE/Models/Product_Posts.cs
@@ -24,7 +24,11 @@ namespace HangKenhFE.Models
 
         [StringLength(20)]
         public string? Status { get; set; }
-
+        public bool? bannernoibat { get; set; }
+        public bool? duannoibat { get; set; }
+        public bool? trendy { get; set; }
+        public bool? baivietnoibat { get; set; }
+        public bool? quytrinh { get; set; }
         public long AuthorId { get; set; }
         public bool? Deleted { get; set; }
 
