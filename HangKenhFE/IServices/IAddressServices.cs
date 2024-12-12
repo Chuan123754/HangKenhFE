86fae1860ac7b04f243851d0e8b106036081eddf
Annt32 <nguyentruongan03022004@gmail.com>
2024-12-12 23:32:15 +0700

    Login, DK. Order, OrderTracking

--- HangKenhFE/IServices/IAddressServices.cs
+++ HangKenhFE/IServices/IAddressServices.cs
@@ -8,6 +8,7 @@ namespace HangKenhFE.IServices
         public Task<List<Address>> GetAddressByUserId(long userId);
         public Task<Address> GetAddressById(long id);
         public Task<bool> CreateAddress(Address address);
+        public Task<Address> CreateAddressAndReturn(Address address);
         public Task UpdateAddress(Address address, long id);
         public Task DeleteAddress(long id);
         public Task SetAsDefault(long id, Address address);
--- HangKenhFE/IServices/IOrderIServices.cs
+++ HangKenhFE/IServices/IOrderIServices.cs
@@ -9,12 +9,22 @@ namespace HangKenhFE.IServices
         Task<List<Orders>> GetOrderByIdAdmin(string idAdmin);
         Task<List<Orders>> GetOrderByIdUser(long idUser);
         Task<Orders> GetByIdOrders(long id);
+        Task<Orders> OrdersAddress(long id);
         Task<Orders> Create(Orders orders);
         Task Update(Orders orders, long id);
+        Task UpdateStatus(Orders orders, long id);
         Task Delete(long id);
         Task<byte[]> ExportInvoice(long orderId);
 
         Task<MomoPaymentResponse> CreateMomoPaymentUrl(string fullName, decimal amount, string orderInfo);
         Task<MomoPaymentResponse> QueryPaymentStatus(string orderId);
+        Task<int> GetTotal();
+        Task<int> GetTotalToday();
+        Task<decimal> GetTotalPiceToday();
+        Task<decimal> GetTotalPiceWeek();
+        Task<decimal> GetTotalPiceMonth();
+        public Task<Dictionary<int, decimal>> GetTotalRevenuePerYear();
+        public Task<Dictionary<string, int>> GetTotalMonth();
+
     }
 }
