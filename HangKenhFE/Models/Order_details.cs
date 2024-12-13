02c6713ddd95673587b66c503cccfdaefbca3804
XuanDung09 <xdug94@gmail.com>
2024-12-14 04:30:23 +0700

    validate số lượng khi thanh toán và thêm vào giỏ hàng

--- HangKenhFE/Models/Order_details.cs
+++ HangKenhFE/Models/Order_details.cs
@@ -17,9 +17,20 @@ namespace HangKenhFE.Models
         public long OrderId { get; set; }
         public long Product_Attribute_Id { get; set; }
         public int Quantity { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public long? TotalDiscount { get; set; }
         [ForeignKey("OrderId")]
+        [JsonIgnore]
         public virtual Orders? Orders { get; set; }
         [ForeignKey("Product_Attribute_Id")]
+        [JsonIgnore]
         public virtual Product_Attributes? ProductAttributes { get; set; }
+        public virtual Products_Returned ProductsReturned { get; set; } = new Products_Returned();
+
+        [NotMapped]
+        public bool IsSelected { get; set; } = false; //tích
+
+        [NotMapped]
+        public int ReturnQuantity { get; set; } = 1;// Số lượng muốn đổi
     }
 }
---
+++ HangKenhFE/Models/Products_Returned.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HangKenhFE.Models
+{
+    public class Products_Returned
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public long OrderDetailId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ReturnReason { get; set; } = string.Empty; // Lý do trả hàng
+        public int Quantity { get; set; }
+
+
+        [Required]
+        [StringLength(50)]
+        public string Condition { get; set; } = string.Empty; // Tình trạng sản phẩm (ví dụ: Hỏng, Sửa được...)
+
+        [Required]
+        public DateTime ReturnDate { get; set; } = DateTime.UtcNow; // Ngày trả hàng
+
+        public string? Notes { get; set; }
+        public DateTime? Created_at { get; set; } = DateTime.Now;
+        public long? Created_by { get; set; }
+        public DateTime? Update_at { get; set; }
+
+        [ForeignKey("OrderDetailId")]
+        public virtual Order_details? OrderDetails { get; set; }
+    }
+}
