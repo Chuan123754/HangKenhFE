3ea7ecc2295edf7d3b8941ab9d7d432ca14b2c2d
XuanDung09 <xdug94@gmail.com>
2025-01-12 02:01:34 +0700

    hủy đơn hoàn voucher

---
+++ HangKenhFE/IServices/IOrderVoucherServices.cs
@@ -0,0 +1,11 @@
+﻿using HangKenhFE.Models;
+
+namespace HangKenhFE.IServices
+{
+    public interface IOrderVoucherServices
+    {
+        Task<List<Order_Vouchers>> GetAll();
+        Task GetByVoucherIdAndOrderId(long orderId, long voucherId);
+        Task<bool> Create(Order_Vouchers order_Voucher);
+    }
+}
---
+++ HangKenhFE/Services/OrderVoucherServices.cs
@@ -0,0 +1,46 @@
+﻿using HangKenhFE.IServices;
+using HangKenhFE.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HangKenhFE.Services
+{
+    public class OrderVoucherServices :IOrderVoucherServices
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl;
+
+        public OrderVoucherServices(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _baseUrl = configuration.GetValue<string>("ApiSettings:BaseUrl");
+        }
+
+        public async Task<bool> Create(Order_Vouchers order_Voucher)
+        {
+            string requestURL = $"{_baseUrl}/api/OrderVouchers/Create";
+            var jsonContent = JsonConvert.SerializeObject(order_Voucher);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(requestURL, content);
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Failed to create UserVoucher. Status Code: {response.StatusCode}, Reason: {response.ReasonPhrase}, Content: {errorContent}");
+            }
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task GetByVoucherIdAndOrderId(long orderId, long voucherId)
+        {
+            await _httpClient.GetStringAsync($"https://localhost:7011/api/OrderVouchers/GetByIdOrderAndIdVoucher?idOrder={orderId}&idVoucher={voucherId}");
+        }
+        public async Task<List<Order_Vouchers>> GetAll()
+        {
+            string requestURL = "https://localhost:7011/api/OrderVouchers";
+            var response = await _httpClient.GetStringAsync(requestURL);
+            var orderVouchers = JsonConvert.DeserializeObject<List<Order_Vouchers>>(response);
+            return orderVouchers;
+        }
+    }
+}
